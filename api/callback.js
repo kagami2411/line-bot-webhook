00ee74991105518b1aa21c4b7f3c8b6c14466eee
kagami2411 <miura.8924@gmail.com>
2025-12-09 11:50:19 +0900

    Update callback.js

--- api/callback.js
+++ api/callback.js
@@ -1,8 +1,27 @@
-export default function handler(req, res) {
-  if (req.method === 'POST') {
-    console.log("LINE Webhook Received!");
-    res.status(200).send("OK");
+import { Client } from "@line/bot-sdk";
+
+const config = {
+  channelAccessToken: process.env.CHANNEL_ACCESS_TOKEN,
+  channelSecret: process.env.CHANNEL_SECRET
+};
+
+const client = new Client(config);
+
+export default async function handler(req, res) {
+  if (req.method === "POST") {
+    const events = req.body.events;
+
+    for (const event of events) {
+      if (event.type === "message") {
+        await client.replyMessage(event.replyToken, {
+          type: "text",
+          text: "こんにちは！Bot稼働中です！🤖✨"
+        });
+      }
+    }
+
+    return res.status(200).send("OK");
   } else {
-    res.status(405).send("Method Not Allowed");
+    return res.status(405).send("Method Not Allowed");
   }
 }
